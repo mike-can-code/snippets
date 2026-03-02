c18eadf12b47b9062d56ddfa059c81bd2f84a93b
mike-can-code <michael.schertz.civ@mail.mil>
2026-03-02 15:49:30 -0600

    Update get_delimiters.py

--- src/wip/get_delimiters.py
+++ src/wip/get_delimiters.py
@@ -1,36 +1,55 @@
-def get_delimiters(edi_dirty_contents: str):
+from typing import Dict, Optional
+
+def get_delimiters(edi_dirty_contents: str) -> Dict[str, Optional[str]]:
+    """
+    Parses an EDI X12 string to extract its delimiter set, handling real-world
+    formatting and truncated ISA segments.
+    """
     
     # -------------  Get start of ISA (In case there is leading characters before the "ISA")
-    isa_start_location = edi_dirty_contents.find('ISA')
-    edi_clean_contents = edi_dirty_contents[isa_start_location:]
+    try:
+        isa_start_index = edi_dirty_contents.index('ISA')
+        edi_clean_contents = edi_dirty_contents[isa_start_index:]
+    except ValueError:
+        raise ValueError("Fatal: Could not find 'ISA' segment in the provided EDI content.")
 
-    # -------------  Get Element Seperator
-    element_seperator = edi_clean_contents[3]
+    # -------------  Get Element Separator 
+    # Element Separator is in position 4 (python is zero based so we use 3)
+    element_separator = edi_clean_contents[3]
 
     # -------------  Get Segment Terminator
-    # If edi sender truncated blank values then "Segment Terminator" will not be in the correct position
+    gs_marker = f'GS{element_separator}'
+    try:
+        gs_index = edi_clean_contents.find(gs_marker)
+        if gs_index == -1:
+            raise ValueError("GS segment marker not found.")
+    except ValueError:
+         raise ValueError("Could not determine segment terminator: GS segment not found.")
+             
     segment_terminator = (edi_clean_contents[105] 
-                          if edi_clean_contents[106:108] == 'GS' 
-                          else edi_clean_contents[edi_clean_contents.find('GS' + element_seperator) -1])
+                         if gs_index == 106
+                         else edi_clean_contents.split(element_separator, 17)[16][1])
+
+    # -------------  Get clean ISA and GS elements
+    isa_line, gs_line = [line.strip() for line in edi_clean_contents.split(segment_terminator, 2)[:2]]
+    isa_elements = isa_line.split(element_separator)
+    gs_elements = gs_line.split(element_separator)
     
-    # -------------  Get ISA and GS elements
-    isa_elements, gs_elements = [_.split(element_seperator) for _ in edi_clean_contents.split(segment_terminator, 2)[:2]]
-
+    
     # -------------  Get Compount Seperator
-    compound_seperator =isa_elements[16]
-
+    compound_seperator = isa_elements[16]
+    
     # -------------  Get X12 Release
     x12_release = int(gs_elements[8])
 
-    # -------------  Get 
-    repetition_seperator = isa_elements[11] if x12_release < 4010 else None
+    # -------------  Get Repetition Seperator
+    repetition_seperator = isa_elements[11] if x12_release > 4010 else None
 
-    delimiters = {'element': element_seperator,
-                  'segment': segment_terminator,
-                  'compound': compound_seperator,
-                  'repetition': repetition_seperator
+    # -------------  Create the delimiter dictionary
+    delimiters = {
+        'element': element_separator,
+        'segment': segment_terminator,
+        'compound': compound_seperator,
+        'repetition': repetition_seperator
     }
     return delimiters
-
-delimiter: dict = get_delimiters(edi_dirty_contents)
-delimiter
